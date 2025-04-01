6e7f833fe917e12c21370ea740fefd7c4b28e854
Hansel Corzo <emirmartinez198@gmail.com>
2025-03-31 23:57:21 -0600

    Cambio directo al título con JavaScript para que se actualice correctamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { Helmet } from "react-helmet";
+import { useEffect } from "react";
 import ProductGallery from "./components/ProductGallery";
 
 function App() {
+  useEffect(() => {
+    document.title = "Catálogo de Gorras Barón Caps";
+  }, []);
+
   return (
     <div className="container">
-      <Helmet>
-        <title>Catálogo de Gorras Barón Caps</title>
-      </Helmet>
       <h1>Gorras Keris Men’s</h1>
       <ProductGallery />
     </div>
