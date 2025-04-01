ffef621524523510200a4bfae21dc6ced48ae673
Hansel Corzo <emirmartinez198@gmail.com>
2025-03-31 23:48:07 -0600

    Título actualizado con react-helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useEffect } from "react";
-import "./index.css";
+import { Helmet } from "react-helmet";
 import ProductGallery from "./components/ProductGallery";
 
 function App() {
-  useEffect(() => {
-    document.title = "Catálogo de Gorras Keris Men’s";
-  }, []);
-
   return (
     <div className="container">
+      <Helmet>
+        <title>Catálogo de Gorras Barón Caps</title>
+      </Helmet>
       <h1>Gorras Keris Men’s</h1>
       <ProductGallery />
     </div>
   );
 }
 
-export default App; 
+export default App;
